0829b031ac6929057f5aeaf6aa30ef9b52ac8eaf
code <code@Adantas-MacBook-Air.local>
2026-04-16 10:49:03 -0400

    deploy edits

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import styles from './App.module.scss';
 import { RouterProvider, createBrowserRouter, createRoutesFromElements, Route } from 'react-router-dom';
 import Root from './Components/Root/root';
 import Home from './Components/Home/home';
